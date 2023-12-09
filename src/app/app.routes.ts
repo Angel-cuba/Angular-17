0ffb2988a44408a4ca91768d4bdc8b813cbe273f
Angel_PC <araozangel842@gmail.com>
2023-12-09 03:12:52 +0200

    Routes DONE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component').then(m => m.ChangeDetectionComponent)
+      },
+      {
+        path: 'control-flow',
+        title: 'Control Flow',
+        loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component').then(m => m.ControlFlowComponent)
+      }
+      , {
+        path: 'defer-option',
+        title: 'Defer Option',
+        loadComponent: () => import('./dashboard/pages/defer-option/defer-option.component').then(m => m.DeferOptionComponent)
+      },
+      {
+        path: 'user/:id',
+        title: 'User View',
+        loadComponent: () => import('./dashboard/pages/user/user.component').then(m => m.UserComponent)
+      },
+      {
+        path: 'users',
+        title: 'Users',
+        loadComponent: () => import('./dashboard/pages/users/users.component').then(m => m.UsersComponent)
+      },
+      {
+        path: 'view-transition',
+        title: 'View Transition',
+        loadComponent: () => import('./dashboard/pages/view-transition/view-transition.component').then(m => m.ViewTransitionComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'control-flow',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
