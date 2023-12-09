4dcfc00961d29e49081595f9f0f7e29e1e6856bc
Angel_PC <araozangel842@gmail.com>
2023-12-09 16:32:14 +0200

    Summary:
    - Dynamic title component.
    - Test boolean variable included in title component.

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-defer-views',
   standalone: true,
-  imports: [],
+  selector: 'app-defer-views',
+  imports: [CommonModule, TitleComponent],
   templateUrl: './defer-views.component.html',
-  styleUrl: './defer-views.component.css'
+  styles: ``,
 })
 export class DeferViewsComponent {
 
