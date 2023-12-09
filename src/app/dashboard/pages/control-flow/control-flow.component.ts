6858e2b4f43da4904a28c55b75ac9f78b4590390
Angel_PC <araozangel842@gmail.com>
2023-12-09 15:43:29 +0200

    Implement @for and @if

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
+
+type Grade = 'A' | 'B' | 'F';
 @Component({
-  selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
-  styleUrl: './control-flow.component.css'
+  styles: ``,
 })
 export class ControlFlowComponent {
+  public showContent = signal(false);
+  public grade= signal<Grade>('B');
 
+  public frameWorks = signal(['Angular', 'React', 'Vue', 'Svelte', 'Ember']);
+  public frameWorks2 = signal([]);
+
+  public toggleContent(): void {
+    this.showContent.update((value) => !value);
+  }
 }
