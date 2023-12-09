28c6704073bac3d040bb46c35da8c208a16706c1
Angel_PC <araozangel842@gmail.com>
2023-12-09 17:31:24 +0200

    State detector property

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,13 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy .OnPush,
   templateUrl: './change-detection.component.html',
   styleUrl: './change-detection.component.css'
 })
 export class ChangeDetectionComponent {
+
+  public currentName = computed(() => this.frameworkasSignal().name);
+
+  public frameworkasSignal = signal({
+    name: 'React',
+    releaseDate: 2013,
+  });
+
+  public frameworkasProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      //this.frameworkasProperty.name = 'Svelte';
+      //this.frameworkasProperty.releaseDate = 2020;
+      this.frameworkasSignal.update( value => ({
+        ...value,
+        name: 'Svelte',
+        releaseDate: 2020
+      }))
+      console.log('ChangeDetectionComponent: setTimeout');
+    }, 3000);
+  }
 
 }
